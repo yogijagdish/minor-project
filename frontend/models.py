0b548fb5d2118302e59c79ea2bf50baaa4608a28
yogijagdish <yogijagdish007@gmail.com>
2022-04-27 11:13:46 +0545

    climate api called

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class climate(models.Model):
+    weather = models.CharField(max_length=40)
+    description = models.CharField(max_length=255)
+    temp = models.DecimalField(max_digits=6,decimal_places=2,primary_key=True)
+    temp_min = models.DecimalField(max_digits=6,decimal_places=2)
+    temp_max = models.DecimalField(max_digits=6,decimal_places=2)
+
+class climate_tilicho(models.Model):
+    weather = models.CharField(max_length=40)
+    description = models.CharField(max_length=255)
+    temp = models.DecimalField(max_digits=6,decimal_places=2,primary_key=True)
+    temp_min = models.DecimalField(max_digits=6,decimal_places=2)
+    temp_max = models.DecimalField(max_digits=6,decimal_places=2)
+
+class climate_langtang(models.Model):
+    weather = models.CharField(max_length=40)
+    description = models.CharField(max_length=255)
+    temp = models.DecimalField(max_digits=6,decimal_places=2,primary_key=True)
+    temp_min = models.DecimalField(max_digits=6,decimal_places=2)
+    temp_max = models.DecimalField(max_digits=6,decimal_places=2)
--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,76 @@
 from django.shortcuts import render
+from . models import climate, climate_langtang, climate_tilicho
+import mysql.connector
 import requests
 
 # Create your views here.
+def api_mardi():
+    try:
+        mydb = mysql.connector.connect(host="localhost",user="jagdish",passwd="Jagdish@1234",database="minor_project")
+        mycursor = mydb.cursor()
+        response = requests.get("https://api.openweathermap.org/data/2.5/weather?q=mardi&appid=b44fcb0e2f09d3ef62d74f75f15c4820").json()
+
+        dist = response['weather']
+        dist1 = response['main']
+        dist2 = dist[0]
+        main = dist2.get('main')
+        description = dist2.get('description')
+        temp = dist1.get('temp')
+        temp_min = dist1.get('temp_min')
+        temp_max = dist1.get('temp_max')
+        lst = [main,description,temp,temp_min,temp_max]
+        
+        query1 = "insert into frontend_climate values(%s,%s,%s,%s,%s)"
+        mycursor.execute(query1,lst)
+        mydb.commit()
+    except:
+        pass
+
+def api_tilicho():
+    try:
+        mydb = mysql.connector.connect(host="localhost",user="jagdish",passwd="Jagdish@1234",database="minor_project")
+        mycursor = mydb.cursor()
+        response = requests.get("https://api.openweathermap.org/data/2.5/weather?q=manang&appid=b44fcb0e2f09d3ef62d74f75f15c4820").json()
+
+        dist = response['weather']
+        dist1 = response['main']
+        dist2 = dist[0]
+        main = dist2.get('main')
+        description = dist2.get('description')
+        temp = dist1.get('temp')
+        temp_min = dist1.get('temp_min')
+        temp_max = dist1.get('temp_max')
+        lst = [main,description,temp,temp_min,temp_max]
+        
+        query1 = "insert into frontend_climate_tilicho values(%s,%s,%s,%s,%s)"
+        mycursor.execute(query1,lst)
+        mydb.commit()
+    except:
+        pass
+    
+def api_langtang():
+    try:
+        mydb = mysql.connector.connect(host="localhost",user="jagdish",passwd="Jagdish@1234",database="minor_project")
+        mycursor = mydb.cursor()
+        response = requests.get("https://api.openweathermap.org/data/2.5/weather?q=langtang&appid=b44fcb0e2f09d3ef62d74f75f15c4820").json()
+
+        dist = response['weather']
+        dist1 = response['main']
+        dist2 = dist[0]
+        main = dist2.get('main')
+        description = dist2.get('description')
+        temp = dist1.get('temp')
+        temp_min = dist1.get('temp_min')
+        temp_max = dist1.get('temp_max')
+        lst = [main,description,temp,temp_min,temp_max]
+        
+        query1 = "insert into frontend_climate_langtang values(%s,%s,%s,%s,%s)"
+        mycursor.execute(query1,lst)
+        mydb.commit()
+    except:
+        pass
+
+
 def home(request):
     return render(request,'pages/home.html',
     {'link1':'http://127.0.0.1:8000/',
@@ -22,28 +91,37 @@ def trek(request):
     'link8':'http://127.0.0.1:8000/frontend/trek/langtang'})
 
 def langtang(request):
+    api_langtang()
+    climate_langtang1 = climate_langtang.objects.all()
     return render(request,'pages/trek/langtang.html',
     {'link1':'http://127.0.0.1:8000/',
     'link2':'http://127.0.0.1:8000/frontend/trek',
     'link3':'http://127.0.0.1:8000/frontend/guide',
     'link4':'http://127.0.0.1:8000/frontend/curentrecord',
-    'link5':'http://127.0.0.1:8000/frontend/contactus'})
+    'link5':'http://127.0.0.1:8000/frontend/contactus',
+    'climate_langtang2':climate_langtang1})
 
 def mardi(request):
+    api_mardi()
+    climate1 = climate.objects.all()
     return render(request,'pages/trek/mardi.html',
     {'link1':'http://127.0.0.1:8000/',
     'link2':'http://127.0.0.1:8000/frontend/trek',
     'link3':'http://127.0.0.1:8000/frontend/guide',
     'link4':'http://127.0.0.1:8000/frontend/curentrecord',
-    'link5':'http://127.0.0.1:8000/frontend/contactus'})
+    'link5':'http://127.0.0.1:8000/frontend/contactus',
+    'climate2':climate1})
 
 def tilicho(request):
-    return render(request,'pages/tilicho.html',
+    api_tilicho()
+    climate_tilicho1 = climate_tilicho.objects.all()
+    return render(request,'pages/trek/tilicho.html',
     {'link1':'http://127.0.0.1:8000/',
     'link2':'http://127.0.0.1:8000/frontend/trek',
     'link3':'http://127.0.0.1:8000/frontend/guide',
     'link4':'http://127.0.0.1:8000/frontend/curentrecord',
-    'link5':'http://127.0.0.1:8000/frontend/contactus'})
+    'link5':'http://127.0.0.1:8000/frontend/contactus',
+    'climate_tilicho2':climate_tilicho1})
 
 def guide(request):
     return render(request,'pages/guide.html',
